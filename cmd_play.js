3e83d5b24d052a6a96b5c066c0a70cfc6255df83
FireC <dontishko2@outlook.com>
2018-08-29 00:25:59 +0300

    OWO

--- cmd_play.js
+++ cmd_play.js
@@ -1,6 +1,7 @@
 // WORK IN PROGRESS (PROBABLY NOT CURRENTLY WORKING CAUSE FIREC IS AN IDIOT)
 var search = require('youtube-search');
 const fs = require('fs');
+const ytdl = require('ytdl-core');
 var opts = {
   maxResults: 1,
   key: 'AIzaSyBXcfJ5kwQ9t8xvppno8yjkJZkc9PwreXs'
@@ -8,10 +9,11 @@ var opts = {
 module.exports = function(param, clientArg, args) { // it sends help
 if (args.length > 1) {
   search(args, opts, function(err, results) {
-    if(err) {
+    if(err) { //log error
       console.log(err);
     }
-    console.log(results)
+console.log(JSON.parse(results))
+
   });
 } else {
 param.channel.createMessage('You have to specify a song! (only works with youtube)');
