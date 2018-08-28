51164e3168c44267d8bb91ea33361e981ef4f214
cth103 <cthdev@email.com>
2018-08-28 16:01:19 +0100

    NSFW CHANEL

--- cmd_r34.js
+++ cmd_r34.js
@@ -6,11 +6,15 @@ module.exports = function(param, clientArg, args) { // it sends help
     request('http://rule34.xxx/index.php?page=dapi&s=post&q=index&tags='+encodeURI(args), function(error, response, body) {
 	let xmlDoc = libxmljs.parseXml(body);
 	let children = xmlDoc.root().childNodes();
-    let child = children[Math.floor(children.length * Math.random())];
+	let child = children[Math.floor(children.length * Math.random())];
 	try {
-    param.channel.createMessage(':weary: :ok_hand: http:' + child.attr('file_url').value());
+		if(param.channel.nsfw) {
+    			param.channel.createMessage(':weary: :ok_hand: http:' + child.attr('file_url').value());
+		} else {
+			param.channel.createMessage('Oops, this is not an NSFW channel :smile:');
+		}
 	} catch (e) {
-	param.channel.createMessage(':rage: :ok_hand: There isn\'t porn out of it! How? :thinking:');
-    }
+		param.channel.createMessage(':rage: :ok_hand: There isn\'t porn out of it! How? :thinking:');
+	}
     });
 };
