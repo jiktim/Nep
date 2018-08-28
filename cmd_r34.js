0651741230966307f4e516a510c3b08b5fb831fd
cth103 <cthdev@email.com>
2018-08-28 19:26:38 +0100

    Update cmd_r34.js

--- cmd_r34.js
+++ cmd_r34.js
@@ -3,7 +3,7 @@ var booru = require('booru');
 // beta
 
 module.exports = function(param, clientArg, args) { // it sends help
-	booru.search(site, [clientArg], {limit: 1, random: true}).then(booru.commonfy).then(images => {
+	booru.search("rule34.xxx", [clientArg], {limit: 1, random: true}).then(booru.commonfy).then(images => {
 		if(param.channel.nsfw) {
 			for (let image of images) {
 				param.channel.createMessage(':weary: :ok_hand: ' + image.common.file_url);
