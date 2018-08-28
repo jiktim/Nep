add2a74c20611a0c60952c1d1c896a4e8d99ac5f
FireC <dontishko2@outlook.com>
2018-08-28 22:53:41 +0300

    made r34 work

--- cmd_r34.js
+++ cmd_r34.js
@@ -4,16 +4,25 @@ var booru = require('booru');
 var argument = new Array();
 var common;
 module.exports = function(param, clientArg, args) { // it sends help
-	argument[0] = clientArg;
-	const search = booru.search('r34', argument, {
-          limit: 1,
-          random: true
-        }),
-        common = booru.commonfy(search);
-	      
-	if(param.channel.nsfw) {
-		param.channel.createMessage(':weary: :ok_hand: ' + common[0]);
-	} else {
-		param.channel.createMessage('This is not an NSFW channel :smiley:');
-	}
-};
+  argument[0] = clientArg;
+  booru.search('r34', [args], {
+      limit: 1,
+      random: true
+    })
+    .then(booru.commonfy)
+    .then(images => {
+      if (param.channel.nsfw) {
+        for (let image of images) {
+          param.channel.createMessage(':weary: :ok_hand: ' + image.common.file_url);
+        }
+
+      } else {
+        param.channel.createMessage('This is not an NSFW channel :smiley:');
+      }
+    })
+    .catch(err => {
+      if (err.name == 'BooruError') {
+        param.channel.createMessage("I couldnt find an image... sorry! 😞");
+      }
+    })
+}
