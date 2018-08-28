a83f7b50fe1876adb5e3f9c4f0e201147017cb86
cth103 <cthdev@email.com>
2018-08-28 19:41:50 +0100

    Update cmd_r34.js

--- cmd_r34.js
+++ cmd_r34.js
@@ -4,18 +4,15 @@ var booru = require('booru');
 var argument = new Array();
 module.exports = function(param, clientArg, args) { // it sends help
 	argument[0] = clientArg;
-	booru.search("rule34.xxx", argument, {limit: 1, random: true})
-		.then(booru.commonfy)
-		.then(images => {
-		if(param.channel.nsfw) {
-			for (let image of images) {
-				param.channel.createMessage(':weary: :ok_hand: ' + image.common.file_url);
-			}
-		} else {
-			param.channel.createMessage('This is not an NSFW channel :smiley:');
-		}
-	}).catch(e => {
-		console.log(e);
-		param.channel.createMessage(':rage: :ok_hand: There isn\'t porn out of it! How? :thinking:');
-	});
+	const search = await booru.search('r34', argument, {
+          limit: 1,
+          random: true
+        }),
+        var common = await booru.commonfy(search);
+	      
+	if(param.channel.nsfw) {
+		param.channel.createMessage(':weary: :ok_hand: ' + common[0].common.file_url);
+	} else {
+		param.channel.createMessage('This is not an NSFW channel :smiley:');
+	}
 };
