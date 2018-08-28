680d01b09d91ba6c96de85ddc91cfbea7fa14442
FireC <dontishko2@outlook.com>
2018-08-28 23:31:15 +0300

    OwO the bot can now look for youtube videos :o

--- cmd_play.js
+++ cmd_play.js
@@ -1,9 +1,19 @@
 // WORK IN PROGRESS (PROBABLY NOT CURRENTLY WORKING CAUSE FIREC IS AN IDIOT)
-
+var search = require('youtube-search');
+const fs = require('fs');
+var opts = {
+  maxResults: 1,
+  key: 'AIzaSyBXcfJ5kwQ9t8xvppno8yjkJZkc9PwreXs'
+};
 module.exports = function(param, clientArg, args) { // it sends help
 if (args.length > 1) {
-param.channel.createMessage('its 4 am i will do this tommrow'); // https://github.com/abalabahaha/eris/blob/master/examples/playFile.js
+  search(args, opts, function(err, results) {
+    if(err) {
+      console.log(err);
+    }
+    console.log(results)
+  });
 } else {
-param.channel.createMessage('You have to specify a song!');
+param.channel.createMessage('You have to specify a song! (only works with youtube)');
 }
 };
