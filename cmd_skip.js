33fce497f0438642e3e089054fe46ad31778144e
FireC <dontishko2@outlook.com>
2018-08-29 22:17:24 +0300

    uwu

---
+++ cmd_skip.js
@@ -0,0 +1,7 @@
+module.exports = function(msg, bot, args) {
+	if (!msg.member.voiceState.channelID) {
+			msg.channel.createMessage('You have to be in a voice channel!');
+	} else {
+    	bot.leaveVoiceChannel(msg.member.voiceState.channelID);
+	}
+}
--- cmd_stop.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(msg, bot, args) {
-	if (!msg.member.voiceState.channelID) {
-			msg.channel.createMessage('You have to be in a voice channel!');
-	} else {
-    	bot.leaveVoiceChannel(msg.member.voiceState.channelID);
-	}
-}
