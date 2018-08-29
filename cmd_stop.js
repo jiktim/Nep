7a6a7a89a5cd8d5bf94f7f4f146fe588f80de082
FireC <dontishko2@outlook.com>
2018-08-29 20:16:06 +0300

    lul no

--- cmd_stop.js
+++ cmd_stop.js
@@ -3,6 +3,5 @@ module.exports = function(msg, bot, args) {
 			msg.channel.createMessage('You have to be in a voice channel!');
 	} else {
     	bot.leaveVoiceChannel(msg.member.voiceState.channelID);
-		msg.channel.createMessage("ok lol")
 	}
 }
