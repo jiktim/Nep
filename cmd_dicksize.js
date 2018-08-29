4fef809f9e43a87f89e8b7b686addeca550de351
cth103 <cthdev@email.com>
2018-08-29 19:02:51 +0100

    Add files via upload

---
+++ cmd_dicksize.js
@@ -0,0 +1,10 @@
+Math.seed = function(s) {
+    return function() {
+        s = Math.sin(s) * 10000; return s - Math.floor(s);
+    };
+};
+module.exports = function(msg, bot, args) {
+	var inches = Math.floor(Math.seed(msg.member.id)() * 30) + 1  * 2
+	var dick = "8"+"=".repeat(inches/2)+"D";
+	msg.channel.createMessage(dick + " ("+inches.toString()+" inches)")
+}
