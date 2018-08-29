cf13abaf6fedf332e20b102219052d1e470a2c77
cth103 <cthdev@email.com>
2018-08-29 19:54:38 +0100

    Update cmd_dicksize.js

--- cmd_dicksize.js
+++ cmd_dicksize.js
@@ -1,5 +1,5 @@
 module.exports = function(msg, bot, args) {
-	var inches = 479379600351166478/100000000000000000*16
+	var inches = msg.member.id/100000000000000000*4
 	var dick = "8"+"=".repeat(inches/2)+"D";
 	msg.channel.createMessage(dick + " ("+inches.toString()+" inches)")
 }
