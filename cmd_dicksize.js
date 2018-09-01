37d4f3979487a0f6e21c348aecf261403932a839
FireC <dontishko2@outlook.com>
2018-09-01 08:03:18 +0300

    Delete cmd_dicksize.js

--- cmd_dicksize.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(msg, bot, args) {
-	var inches = msg.member.id/100000000000000000*4
-	var dick = "8"+"=".repeat(inches/2)+"D";
-	msg.channel.createMessage(dick + " ("+inches.toString()+" inches)")
-}
