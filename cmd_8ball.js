92f600a5bbec7f79d2d57315cd3cc75e3a16c6dc
FireC <dontishko2@outlook.com>
2018-09-01 08:02:54 +0300

    Delete cmd_8ball.js

--- cmd_8ball.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(param, clientArg, args) {
-let responses = ['It is certain', 'Without a doubt', 'You may rely on it', 'Most likely', 'Yes', 'Signs point to yes', 'Better not tell you now', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
-param.channel.createMessage(':8ball: | `'+responsess[Math.floor(Math.random() * (responsess.length))]+'`');
-};
